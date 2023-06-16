d49e66e969c1e388c4383893d6ed5da73a6fd652
rvp15 <110572735+rvp15@users.noreply.github.com>
2023-06-16 14:13:04 -0400

    class and objects

---
+++ create_Class_Obj.py
@@ -0,0 +1,10 @@
+# OOPS:
+
+class Computer:
+    def config(self):
+        print('i5,16gb,1TB')
+
+# create object
+obj1 = Computer()
+# access object
+obj1.config()
---
+++ patternprogram.py
@@ -0,0 +1,18 @@
+# ascending
+for i in range(5):
+    for j in range(i):
+        print(' #', end='')
+    print('\n')
+
+# Descending
+for i in range(4):
+    for j in range(4-i):
+        print(' #', end='')
+    print('\n')
+
+# square
+for i in range(4):
+    for j in range(4):
+        print(' #', end='')
+    print('\n')
+
